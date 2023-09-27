a90a10c16eb5ab4a24bce4be45cafe18940a3834
ausmanki <manikndn369@gmail.com>
2023-09-27 15:25:41 +0530

    updated userCheck.java

--- src/main/java/com/example/userCheck.java
+++ src/main/java/com/example/userCheck.java
@@ -40,9 +40,9 @@ public class userCheck extends HttpServlet {
             out.println("<body>");
             String user=request.getParameter("user");
           
-            String q = "SELECT r FROM User r where r.UserId='"+user+"'";
-            Query query = em.createQuery(q);
-            
+            Query query=em.createNamedQuery("User.findByUserId");
+            query.setParameter("userId", user);
+
             @SuppressWarnings("unchecked")
         List<User> users = query.getResultList();
            
